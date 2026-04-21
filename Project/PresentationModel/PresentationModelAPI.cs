f2b47648257ae5e1e12dd96abbe339125df4cbd1
Piotr Matuszczyk <254814@edu.p.lodz.pl>
2026-04-21 20:42:30 +0200

    Presentation Model

---
+++ Project/PresentationModel/PresentationModelAPI.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogic;
+using Data;
+
+namespace PresentationModel
+{
+    public class PresentationModelAPI : PresentationModelAbstractAPI
+    {
+        private LogicAbstractAPI _logicAPI;
+
+        public PresentationModelAPI(LogicAbstractAPI logicAPI)
+        {
+            _logicAPI = logicAPI;
+        }
+
+        public override IEnumerable<IBall> GetBalls() => _logicAPI.GetBalls();
+
+        public override void MoveBalls()
+        {
+            _logicAPI.Step();
+        }
+
+        public override void StartSimulation(int BallCount, double BoardWidth, double BoardHight)
+        {
+            _logicAPI.StartSimulation(BallCount, BoardWidth, BoardHight);
+        }
+
+    }
+}
---
+++ Project/PresentationModel/PresentationModelAbstractAPI.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogic;
+using Data;
+
+namespace PresentationModel
+{
+    public abstract class PresentationModelAbstractAPI
+    {
+        public static PresentationModelAbstractAPI CreateAPI(LogicAbstractAPI? api = null)
+        {
+            return new PresentationModelAPI(api ?? LogicAbstractAPI.CreateAPI());
+        }
+
+        public abstract void StartSimulation(int BallCount, double BoardWidth, double BoardHight);
+
+        public abstract IEnumerable<IBall> GetBalls();
+
+        public abstract void MoveBalls();
+    }
+}
